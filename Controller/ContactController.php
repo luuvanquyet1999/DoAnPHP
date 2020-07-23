d3f24c39c2e643ed585421c77e7b3230afc0ccc2
luuvanquyet1999 <quyet40873@donga.edu.vn>
2020-07-23 11:16:03 +0700

    quyet

---
+++ Controller/ContactController.php
@@ -0,0 +1,8 @@
+<?php
+class ContactController
+{
+    function View(){
+        require SYSTEM_PATH."/View/Admin/Contact/List.php";
+    }
+}
+
---
+++ Controller/SlideController.php
@@ -0,0 +1,8 @@
+<?php
+class SlideController
+{
+    function View(){
+        require SYSTEM_PATH."/View/Admin/Slide/List.php";
+    }
+}
+
